36c7d47ec5ab1bd66b01910c0eeebe1ab799d93e
vinod-opps <76504856+vinod-opps@users.noreply.github.com>
2026-07-27 20:12:10 +0530

    Auto sync 27-07-2026 20:12:10.32

--- PATTERNS/LEVEL 3/LEVEL_3.java
+++ PATTERNS/LEVEL 3/LEVEL_3.java
@@ -79,9 +79,49 @@ class ButterFlyPattern{
         }
     }
 }
+
+class HollowButterFlyPattern{
+    static void print(){
+        Main : 
+        while (true){
+            Scanner input = new Scanner(System.in);
+            System.out.print("ENTER THE NUMBER : ");
+            int number = input.nextInt();
+            if (number>0){
+                int k1 = 1;
+                int k2 = number;
+                for ( int i = 1 ; i <= number;i++){
+                    for ( int j = 1 ; j <= number ; j++){
+                        if (j==k1 || j==k2){
+                            System.out.print("*");
+                        }
+                        else{
+                            System.out.print(" ");
+                        }
+                    }
+                    System.out.println("");
+                    if (i<number/2){
+                        k1++;
+                        k2--;
+                    }
+                    else{
+                        k1--;
+                        k2++;
+                    }
+                }
+            }
+            else {
+                System.out.print("TRY AGAIN ");
+            }
+            break Main;
+        }
+    }
+}
+
 public class LEVEL_3{
     public static void main(String args[]){
         // HollowDiamond.print();
         // ButterFlyPattern.print();
+        HollowButterFlyPattern.print();E
     }
 }
