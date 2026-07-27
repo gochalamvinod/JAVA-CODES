b6221d305361cb33240fa3cf8bb97f66d9238c54
vinod-opps <76504856+vinod-opps@users.noreply.github.com>
2026-07-27 12:22:30 +0530

    Initial commit

---
+++ BASICS/PATTERNS/Main.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+class SolidSquare{
+    static void print(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER OF LINES : ");
+        int number = input.nextInt();
+        for (int i = 1;i<=number;i++){
+            for ( int j = 1;j<=number;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    } 
+}
+
+
+
+class IncreasingStarTriangle{
+    static void print(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER OF LINES : ");
+        int number = input.nextInt();
+         for (int i =1;i<=number;i++){
+            System.out.println("* ".repeat(i));
+        }
+    }
+}
+
+class DecreasingStarTriangle{
+    static void print(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER OF LINES : ");
+        int number = input.nextInt();
+        while (number>0){
+            System.out.println("* ".repeat(number));
+            number--;
+        }
+    }
+}
+
+class IncreasingNumberTriangle{
+    static void print(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER OF LINES : ");
+        int number = input.nextInt();
+        for (int i = 1 ; i<=number ; i++){
+            for ( int j =1 ; j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+class RepeatedNumberTriangle{
+    static void print(){
+        Scanner input = new Scanner (System.in);
+        System.out.print("ENTER THE NUMBER OF LINES : ");
+        int number = input.nextInt();
+        for (int i = 1 ; i<=number;i++){
+            System.out.println(Integer.toString(i).repeat(i));
+        }
+        
+    }
+}
+
+class AlphabetTriangle{
+    static void print(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER OF LINES : ");
+    }
+}
+
+class Main{
+    public static void main(String args[]){
+        // SolidSquare.print();
+        // IncreasingStarTriangle.print();
+        // DecreasingStarTriangle.print();
+        // IncreasingNumberTriangle.print();
+        // RepeatedNumberTriangle.print();
+        AlphabetTriangle.print();
+    }
+}
