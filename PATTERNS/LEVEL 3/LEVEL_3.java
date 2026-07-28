07111c497fbc7c1f34aa95b5d08fa0b7eecc9de8
vinod-opps <76504856+vinod-opps@users.noreply.github.com>
2026-07-28 09:53:11 +0530

    Auto sync 28-07-2026  9:53:11.10

--- PATTERNS/LEVEL 3/LEVEL_3.java
+++ PATTERNS/LEVEL 3/LEVEL_3.java
@@ -204,13 +204,397 @@ class FloydTriangle{
         }
     }
 }
+class ReverseFloydTriangle{
+    static void print(){
+        Main:
+        while (true){
+            Scanner input = new Scanner(System.in);
+            System.out.print("ENTER THE NUMBER : ");
+            int number = input.nextInt();
+
+            if (number>0){
+                int val = (number*(number+1))/2;
+
+                for (int i=number ; i>=1 ; i--){
+                    for (int j=1 ; j<=i ; j++){
+                        System.out.print(val+" ");
+                        val--;
+                    }
+                    System.out.println("");
+                }
+                break Main;
+            }
+            else{
+                System.out.println("TRY WITH POSITIVE NUMBER");
+            }
+        }
+    }
+}
+
+
+class PalindromeNumberPyramid{
+    static void print(){
+        Main:
+        while (true){
+            Scanner input = new Scanner(System.in);
+            System.out.print("ENTER THE NUMBER : ");
+            int number=input.nextInt();
+
+            if (number>0){
+
+                for(int i=1 ; i<=number ; i++){
+
+                    for(int j=1 ; j<=number-i ; j++){
+                        System.out.print(" ");
+                    }
+
+                    for(int j=i ; j>=1 ; j--){
+                        System.out.print(j);
+                    }
+
+                    for(int j=2 ; j<=i ; j++){
+                        System.out.print(j);
+                    }
+
+                    System.out.println("");
+                }
+                break Main;
+            }
+            else{
+                System.out.println("TRY WITH POSITIVE NUMBER");
+            }
+        }
+    }
+}
+
+
+class PalindromeAlphabetPyramid{
+    static void print(){
+        Main:
+        while(true){
+            Scanner input=new Scanner(System.in);
+            System.out.print("ENTER THE NUMBER : ");
+            int number=input.nextInt();
+
+            if(number>0){
+
+                for(int i=1 ; i<=number ; i++){
+
+                    for(int j=1 ; j<=number-i ; j++){
+                        System.out.print(" ");
+                    }
+
+                    for(int j=i ; j>=1 ; j--){
+                        System.out.print((char)('A'+j-1));
+                    }
+
+                    for(int j=2 ; j<=i ; j++){
+                        System.out.print((char)('A'+j-1));
+                    }
+
+                    System.out.println("");
+                }
+
+                break Main;
+            }
+            else{
+                System.out.println("TRY WITH POSITIVE NUMBER");
+            }
+        }
+    }
+}
+
+
+class NumberHourGlass{
+    static void print(){
+        Main:
+        while(true){
+            Scanner input=new Scanner(System.in);
+            System.out.print("ENTER THE NUMBER : ");
+            int number=input.nextInt();
+
+            if(number>0){
+
+                for(int i=number ; i>=1 ; i--){
+
+                    for(int j=1 ; j<=number-i ; j++){
+                        System.out.print(" ");
+                    }
+
+                    for(int j=1 ; j<=i ; j++){
+                        System.out.print(i+" ");
+                    }
+
+                    System.out.println("");
+                }
+
+                for(int i=2 ; i<=number ; i++){
+
+                    for(int j=1 ; j<=number-i ; j++){
+                        System.out.print(" ");
+                    }
+
+                    for(int j=1 ; j<=i ; j++){
+                        System.out.print(i+" ");
+                    }
+
+                    System.out.println("");
+                }
+
+                break Main;
+            }
+            else{
+                System.out.println("TRY WITH POSITIVE NUMBER");
+            }
+        }
+    }
+}
+
+
+class StarHourGlass{
+    static void print(){
+        Main:
+        while(true){
+            Scanner input=new Scanner(System.in);
+            System.out.print("ENTER THE NUMBER : ");
+            int number=input.nextInt();
+
+            if(number>0){
+
+                for(int i=number ; i>=1 ; i--){
+
+                    for(int j=1 ; j<=number-i ; j++){
+                        System.out.print(" ");
+                    }
+
+                    for(int j=1 ; j<=i ; j++){
+                        System.out.print("* ");
+                    }
+
+                    System.out.println("");
+                }
+
+                for(int i=2 ; i<=number ; i++){
+
+                    for(int j=1 ; j<=number-i ; j++){
+                        System.out.print(" ");
+                    }
+
+                    for(int j=1 ; j<=i ; j++){
+                        System.out.print("* ");
+                    }
+
+                    System.out.println("");
+                }
+
+                break Main;
+            }
+            else{
+                System.out.println("TRY WITH POSITIVE NUMBER");
+            }
+        }
+    }
+}
+
+
+class HollowHourGlass{
+    static void print(){
+        Main:
+        while(true){
+            Scanner input=new Scanner(System.in);
+            System.out.print("ENTER THE NUMBER : ");
+            int number=input.nextInt();
+
+            if(number>0){
+
+                for(int i=number ; i>=1 ; i--){
+
+                    for(int j=1 ; j<=number-i ; j++){
+                        System.out.print(" ");
+                    }
+
+                    for(int j=1 ; j<=i ; j++){
+
+                        if(i==number || j==1 || j==i){
+                            System.out.print("* ");
+                        }
+                        else{
+                            System.out.print("  ");
+                        }
+                    }
+
+                    System.out.println("");
+                }
+
+
+                for(int i=2 ; i<=number ; i++){
+
+                    for(int j=1 ; j<=number-i ; j++){
+                        System.out.print(" ");
+                    }
+
+                    for(int j=1 ; j<=i ; j++){
+
+                        if(i==number || j==1 || j==i){
+                            System.out.print("* ");
+                        }
+                        else{
+                            System.out.print("  ");
+                        }
+                    }
+
+                    System.out.println("");
+                }
+
+                break Main;
+            }
+            else{
+                System.out.println("TRY WITH POSITIVE NUMBER");
+            }
+        }
+    }
+}
+
+
+class ContinuousAlphabetTriangle{
+    static void print(){
+        Main:
+        while(true){
+            Scanner input=new Scanner(System.in);
+            System.out.print("ENTER THE NUMBER : ");
+            int number=input.nextInt();
+
+            if(number>0){
+
+                char val='A';
+
+                for(int i=1 ; i<=number ; i++){
+
+                    for(int j=1 ; j<=i ; j++){
+
+                        System.out.print(val+" ");
+
+                        val++;
+
+                        if(val>'Z'){
+                            val='A';
+                        }
+                    }
+
+                    System.out.println("");
+                }
+
+                break Main;
+            }
+            else{
+                System.out.println("TRY WITH POSITIVE NUMBER");
+            }
+        }
+    }
+}
+
+
+class MultiplicationTriangle{
+    static void print(){
+        Main:
+        while(true){
+            Scanner input=new Scanner(System.in);
+            System.out.print("ENTER THE NUMBER : ");
+            int number=input.nextInt();
+
+            if(number>0){
+
+                for(int i=1 ; i<=number ; i++){
+
+                    for(int j=1 ; j<=i ; j++){
+
+                        System.out.print((i*j)+" ");
+                    }
+
+                    System.out.println("");
+                }
+
+                break Main;
+            }
+            else{
+                System.out.println("TRY WITH POSITIVE NUMBER");
+            }
+        }
+    }
+}
+
+
+class NumberDiamond{
+    static void print(){
+        Main:
+        while(true){
+            Scanner input=new Scanner(System.in);
+            System.out.print("ENTER THE NUMBER : ");
+            int number=input.nextInt();
+
+            if(number>0){
+
+                for(int i=1 ; i<=number ; i++){
+
+                    for(int j=1 ; j<=number-i ; j++){
+                        System.out.print(" ");
+                    }
+
+                    for(int j=1 ; j<=i ; j++){
+                        System.out.print(j);
+                    }
+
+                    for(int j=i-1 ; j>=1 ; j--){
+                        System.out.print(j);
+                    }
+
+                    System.out.println("");
+                }
+
+
+                for(int i=number-1 ; i>=1 ; i--){
+
+                    for(int j=1 ; j<=number-i ; j++){
+                        System.out.print(" ");
+                    }
+
+                    for(int j=1 ; j<=i ; j++){
+                        System.out.print(j);
+                    }
+
+                    for(int j=i-1 ; j>=1 ; j--){
+                        System.out.print(j);
+                    }
+
+                    System.out.println("");
+                }
+
+                break Main;
+            }
+            else{
+                System.out.println("TRY WITH POSITIVE NUMBER");
+            }
+        }
+    }
+}
 public class LEVEL_3{
     public static void main(String args[]){
-        // HollowDiamond.print();
-        // ButterFlyPattern.print();
-        // HollowButterFlyPattern.print();
-        // X_Pattern.print();
-        // PlusPattern.print();
+
+        HollowDiamond.print();
+        ButterFlyPattern.print();
+        HollowButterFlyPattern.print();
+        X_Pattern.print();
+        PlusPattern.print();
         FloydTriangle.print();
+
+        ReverseFloydTriangle.print();
+        PalindromeNumberPyramid.print();
+        PalindromeAlphabetPyramid.print();
+        NumberHourGlass.print();
+        StarHourGlass.print();
+        HollowHourGlass.print();
+        ContinuousAlphabetTriangle.print();
+        MultiplicationTriangle.print();
+        NumberDiamond.print();
     }
 }
