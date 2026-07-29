6689af44948bd6259baacf279989e564d9016e1c
vinod-opps <76504856+vinod-opps@users.noreply.github.com>
2026-07-29 20:54:51 +0530

    Auto sync 29-07-2026 20:54:51.90

---
+++ PATTERNS/LEVEL 4/LEVEL_4.java
@@ -0,0 +1,198 @@
+import java.util.*;
+
+class PascalTriangle{
+    static void print(){
+    Scanner Input = new Scanner(System.in);
+    Main:
+    while (true){
+        System.out.print("ENTER THE NUMBER : ");
+        int number = Input.nextInt();
+        if (number > 0){
+
+            break Main;
+        }
+        else {
+            System.out.print("ENTER THE NUMBER GREATER THAN 0");
+        }
+    }
+    }
+}
+
+class NumberSpiral{
+    static void print(){
+    Scanner Input = new Scanner(System.in);
+    Main:
+    while (true){
+        System.out.print("ENTER THE NUMBER : ");
+        int number = Input.nextInt();
+        if (number > 0){
+
+            break Main;
+        }
+        else {
+            System.out.print("ENTER THE NUMBER GREATER THAN 0");
+        }
+    }
+    }
+}
+
+class MatrixBoundry{
+    static void print(){
+    Scanner Input = new Scanner(System.in);
+    Main:
+    while (true){
+        System.out.print("ENTER THE NUMBER : ");
+        int number = Input.nextInt();
+        if (number > 0){
+
+            break Main;
+        }
+        else {
+            System.out.print("ENTER THE NUMBER GREATER THAN 0");
+        }
+    }
+    }
+}
+
+class SquareMatrix{
+    static void print(){
+    Scanner Input = new Scanner(System.in);
+    Main:
+    while (true){
+        System.out.print("ENTER THE NUMBER : ");
+        int number = Input.nextInt();
+        if (number > 0){
+
+            break Main;
+        }
+        else {
+            System.out.print("ENTER THE NUMBER GREATER THAN 0");
+        }
+    }
+    }
+}
+
+class Zig_Zag{
+    static void print(){
+    Scanner Input = new Scanner(System.in);
+    Main:
+    while (true){
+        System.out.print("ENTER THE NUMBER : ");
+        int number = Input.nextInt();
+        if (number > 0){
+
+            break Main;
+        }
+        else {
+            System.out.print("ENTER THE NUMBER GREATER THAN 0");
+        }
+    }
+    }
+}
+
+class Wave{
+    static void print(){
+    Scanner Input = new Scanner(System.in);
+    Main:
+    while (true){
+        System.out.print("ENTER THE NUMBER : ");
+        int number = Input.nextInt();
+        if (number > 0){
+
+            break Main;
+        }
+        else {
+            System.out.print("ENTER THE NUMBER GREATER THAN 0");
+        }
+    }
+    }
+}
+
+class SnakeMatrix{
+    static void print(){
+    Scanner Input = new Scanner(System.in);
+    Main:
+    while (true){
+        System.out.print("ENTER THE NUMBER : ");
+        int number = Input.nextInt();
+        if (number > 0){
+
+            break Main;
+        }
+        else {
+            System.out.print("ENTER THE NUMBER GREATER THAN 0");
+        }
+    }
+    }
+}
+
+class ConcentricNumberSquare{
+    static void print(){
+    Scanner Input = new Scanner(System.in);
+    Main:
+    while (true){
+        System.out.print("ENTER THE NUMBER : ");
+        int number = Input.nextInt();
+        if (number > 0){
+
+            break Main;
+        }
+        else {
+            System.out.print("ENTER THE NUMBER GREATER THAN 0");
+        }
+    }
+    }
+}
+
+class ConcentricAplhabetSquare{
+    static void print(){
+    Scanner Input = new Scanner(System.in);
+    Main:
+    while (true){
+        System.out.print("ENTER THE NUMBER : ");
+        int number = Input.nextInt();
+        if (number > 0){
+
+            break Main;
+        }
+        else {
+            System.out.print("ENTER THE NUMBER GREATER THAN 0");
+        }
+    }
+    }
+}
+
+class SprialOrder{
+    static void print(){
+    Scanner Input = new Scanner(System.in);
+    Main:
+    while (true){
+        System.out.print("ENTER THE NUMBER : ");
+        int number = Input.nextInt();
+        if (number > 0){
+
+            break Main;
+        }
+        else {
+            System.out.print("ENTER THE NUMBER GREATER THAN 0");
+        }
+    }
+    }
+}
+
+public class LEVEL_4{
+    public static void main(String[] args){
+
+        // Call any pattern you want
+        PascalTriangle.print();
+        // NumberSpiral.print();
+        // MatrixBoundry.print();
+        // SquareMatrix.print();
+        // Zig_Zag.print();
+        // Wave.print();
+        // SnakeMatrix.print();
+        // ConcentricNumberSquare.print();
+        // ConcentricAplhabetSquare.print();
+        // SprialOrder.print();
+    }
+}
